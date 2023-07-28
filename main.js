0b421d66198069dc743e9d33b34911a915e2fec4
Jaime Sanchez <118962371+Jaime-Sanz@users.noreply.github.com>
2023-07-27 20:31:12 -0700

    function
    fixed function but still needs to be changed

--- main.js
+++ main.js
@@ -7,3 +7,19 @@ function audioVolume(){
     (song.paused) ? audio.style.backgroundImage = "url('images/mute.png')": 
         audio.style.backgroundImage = "url('images/speaker.png')";
 }
+
+const gitRead = () => {
+    window.open("https://github.com/Jaime-Sanz/RockPaperScissors/tree/main")
+}
+
+const gameSelection = () => {
+    var rock = document.getElementById("button1").addEventListener("click", function() {
+        playerCard.src = "images/Rock.png";
+    });
+    var paper = document.getElementById("button2");
+    var scissor = document.getElementById("button3");
+    var playerCard = document.getElementById("playerQ");
+
+
+    
+}
